86161c9e2dd2eccc448bad1884cb1a15eb3fd27d
Sergey Brykulskii <92694237+SergeyBrykulskii@users.noreply.github.com>
2023-09-08 12:05:54 +0300

    added lab2

---
+++ Web_153501_Brykulskii/Web_153501_Brykulskii/Models/ListDemo.cs
@@ -0,0 +1,7 @@
+﻿namespace Web_153501_Brykulskii.Models;
+
+public class ListDemo
+{
+    public int? Id { get; set; }
+    public string? Name { get; set; }
+}
---
+++ Web_153501_Brykulskii/Web_153501_Brykulskii/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_153501_Brykulskii.ViewComponents;
+
+public class CartViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return await Task.FromResult<IViewComponentResult>(View());
+    }
+}
