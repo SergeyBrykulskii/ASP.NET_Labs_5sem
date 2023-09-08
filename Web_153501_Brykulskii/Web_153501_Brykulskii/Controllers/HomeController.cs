b0e5767e8f19b269459172680528c3b8d23c6270
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-09-08 12:04:01 +0300

    added lab2

--- Web_153501_Brykulskii/Web_153501_Brykulskii/Controllers/HomeController.cs
+++ Web_153501_Brykulskii/Web_153501_Brykulskii/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web_153501_Brykulskii.Models;
 
-namespace Web_153501_Brykulskii.Controllers
+namespace Web_153501_Brykulskii.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
+        var listDemo = new List<ListDemo>
         {
-            return View();
-        }
+            new ListDemo { Id =  1, Name = "Item 1"},
+            new ListDemo { Id = 2, Name = "Item 2"},
+            new ListDemo { Id = 3, Name = "Item 3"}
+        };
+
+        ViewData["MainHeaderMessage"] = "Лабораторная работа №2";
+        ViewData["ItemsDemoList"] = listDemo;
+
+        return View();
     }
 }
