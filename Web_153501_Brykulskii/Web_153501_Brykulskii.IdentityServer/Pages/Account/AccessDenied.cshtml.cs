4e9e84f168c4beca8c0e4735530008bef0b5b17e
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-10-24 12:35:36 +0300

    added IdentityServer

---
+++ Web_153501_Brykulskii/Web_153501_Brykulskii.IdentityServer/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_153501_Brykulskii.IdentityServer.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
