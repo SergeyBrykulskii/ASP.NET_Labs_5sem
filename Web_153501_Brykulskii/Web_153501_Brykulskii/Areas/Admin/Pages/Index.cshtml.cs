05912851dd5aebf2896976e4f6caca49d515ba0f
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-10-29 20:26:26 +0300

    added ajax

--- Web_153501_Brykulskii/Web_153501_Brykulskii/Areas/Admin/Pages/Index.cshtml.cs
+++ Web_153501_Brykulskii/Web_153501_Brykulskii/Areas/Admin/Pages/Index.cshtml.cs
@@ -1,32 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Web_153501_Brykulskii.Domain.Entities;
+using Web_153501_Brykulskii.Extensions;
 using Web_153501_Brykulskii.Services.PictureService;
 
 namespace Web_153501_Brykulskii.Areas.Admin.Pages
 {
-    public class IndexModel : PageModel
-    {
-        private readonly IPictureService _pictureService;
+	public class IndexModel : PageModel
+	{
+		private readonly IPictureService _pictureService;
 
-        public IndexModel(IPictureService pictureService)
-        {
-            _pictureService = pictureService;
-        }
+		public IndexModel(IPictureService pictureService)
+		{
+			_pictureService = pictureService;
+		}
 
-        public IList<Picture> Pictures { get; set; } = default!;
-        public int CurrentPage { get; set; }
-        public int TotalPages { get; set; }
+		public IList<Picture> Pictures { get; set; } = default!;
+		public int CurrentPage { get; set; }
+		public int TotalPages { get; set; }
 
-        public async Task OnGetAsync(int pageNo = 1)
-        {
-            var response = await _pictureService.GetPictureListAsync(null, pageNo);
+		public async Task<IActionResult> OnGetAsync(int pageNo = 1)
+		{
+			var response = await _pictureService.GetPictureListAsync(null, pageNo);
 
-            if (response.Success)
-            {
-                Pictures = response.Data!.Items!;
-                CurrentPage = response.Data.CurrentPage;
-                TotalPages = response.Data.TotalPages;
-            }
-        }
-    }
+			if (response.Success)
+			{
+				Pictures = response.Data!.Items!;
+				CurrentPage = response.Data.CurrentPage;
+				TotalPages = response.Data.TotalPages;
+			}
+
+			if (Request.isAjaxRequest())
+			{
+				return Partial("Partials/_PictureListPartial", new
+				{
+					Pictures,
+					CurrentPage,
+					TotalPages,
+					ReturnUrl = Request.Path + Request.QueryString.ToUriComponent(),
+					IsAdmin = true
+				});
+			}
+
+			return Page();
+		}
+	}
 }
--- Web_153501_Brykulskii/Web_153501_Brykulskii/Controllers/ProductController.cs
+++ Web_153501_Brykulskii/Web_153501_Brykulskii/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Web_153501_Brykulskii.Converters;
+using Web_153501_Brykulskii.Extensions;
 using Web_153501_Brykulskii.Services.PictureGenreService;
 using Web_153501_Brykulskii.Services.PictureService;
 
@@ -7,29 +8,42 @@ namespace Web_153501_Brykulskii.Controllers;
 
 public class ProductController : Controller
 {
-    private readonly IPictureGenreService _pictureGenreService;
-    private readonly IPictureService _pictureService;
+	private readonly IPictureGenreService _pictureGenreService;
+	private readonly IPictureService _pictureService;
 
-    public ProductController(
-        IPictureGenreService pictureGenreService,
-        IPictureService pictureService)
-    {
-        _pictureGenreService = pictureGenreService;
-        _pictureService = pictureService;
-    }
-    public async Task<IActionResult> Index(string? genre, int pageNo = 1)
-    {
-        var genresResponse = await _pictureGenreService.GetPictureGenreListAsync();
-        var pictureResponse = await _pictureService.GetPictureListAsync(genre, pageNo);
+	public ProductController(
+		IPictureGenreService pictureGenreService,
+		IPictureService pictureService)
+	{
+		_pictureGenreService = pictureGenreService;
+		_pictureService = pictureService;
+	}
+	public async Task<IActionResult> Index(string? genre, int pageNo = 1)
+	{
+		var genresResponse = await _pictureGenreService.GetPictureGenreListAsync();
+		var pictureResponse = await _pictureService.GetPictureListAsync(genre, pageNo);
 
-        if (!pictureResponse.Success || !genresResponse.Success)
-        {
-            return NotFound(pictureResponse.ErrorMessage + '\n' + genresResponse.ErrorMessage);
-        }
+		if (!pictureResponse.Success || !genresResponse.Success)
+		{
+			return NotFound(pictureResponse.ErrorMessage + '\n' + genresResponse.ErrorMessage);
+		}
 
-        ViewData["genres"] = genresResponse.Data;
-        ViewData["currentGenre"] = GenreConverter.ConvertToRu(genre);
+		ViewData["genres"] = genresResponse.Data;
+		ViewData["currentGenre"] = GenreConverter.ConvertToRu(genre);
 
-        return View((pictureResponse.Data!.Items, pictureResponse.Data.CurrentPage, pictureResponse.Data.TotalPages));
-    }
+		if (Request.isAjaxRequest())
+		{
+			return PartialView("Partials/_PictureListPartial", new
+			{
+				Pictures = pictureResponse.Data!.Items,
+				Genre = genre,
+				pictureResponse.Data.CurrentPage,
+				pictureResponse.Data.TotalPages,
+				ReturnUrl = Request.Path + Request.QueryString.ToUriComponent(),
+				IsAdmin = false
+			});
+		}
+
+		return View((pictureResponse.Data!.Items, pictureResponse.Data.CurrentPage, pictureResponse.Data.TotalPages));
+	}
 }
